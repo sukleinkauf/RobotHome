f919b5af9307323b8ad2934622c03ce884257aa4
Geison Camara <geison.camara@digitaldesk.com.br>
2017-04-04 14:44:08 -0300

    led

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var rpio = require('rpio');
 
+rpio.open(12, rpio.OUTPUT, rpio.LOW);
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/:status', function(req, res, next) {
+	console.log(req.params.status);
+	res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
