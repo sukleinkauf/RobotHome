2a66260e2a9dcfefe12fced94d08afb7c08f2333
Geison Camara <geison.camara@digitaldesk.com.br>
2017-04-04 16:47:04 -0300

    led

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,7 +1,7 @@
 var url = 'http://192.168.1.156:3000/';
 
 function setRouter(elem, action){
-	$.get(url, {status: action}, function(){
+	$.post(url, {status: action}, function(){
 		$(elem).toggleClass('active');
 		alert('ok');
 	});
