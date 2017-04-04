f346f02508ce62bdc93901e86c966a931e4f3f65
Geison Camara <geison.camara@digitaldesk.com.br>
2017-04-04 16:10:20 -0300

    led

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,6 +1,10 @@
 function verifyClass(elem){
 	var check = $(elem).hasClass('active');
-	console.log(check);
+	if(check){
+		window.location.href = 'http://192.168.1.156:3000/off';
+	}else{
+		window.location.href = 'http://192.168.1.156:3000/on';
+	}
 }
 
 function actionClick(){
