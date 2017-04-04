5b97bdad4ac335ae678291a2f94c82a434947cf1
Geison Camara <geison.camara@digitaldesk.com.br>
2017-04-04 16:05:28 -0300

    led

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,10 +1,12 @@
-function ajax(){
-	
+function verifyClass(elem){
+	var check = $(elem).hasClass(active);
+	console.log(check);
 }
 
 function actionClick(){
 	$('.button').click(function(){
 		$(this).toggleClass('active');
+		verifyClass($(this));
 	});
 }
 
