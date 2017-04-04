da0189c52bfcdd7402bdd98e1ce9b2e2fe2c452d
Geison Camara <geison.camara@digitaldesk.com.br>
2017-04-04 15:42:12 -0300

    led

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,18 @@ var Gpio = require('onoff').Gpio;
 var led = new Gpio(14, 'out');
 
 /* GET home page. */
-router.get('/:status', function(req, res, next) {
+router.get('/', function(req, res, next) {
 	console.log(req.params.status);
-	res.render('index', { title: 'Express' });
+	res.render('index', { status: '' });
+});
+
+router.get('/:status', function(req, res, next) {
+	if(status=='on'){
+		led.writeSync(1);
+	}else if(status=='off'){
+		led.writeSync(0);
+	}
+	res.render('index', { status: status });
 });
 
 module.exports = router;
