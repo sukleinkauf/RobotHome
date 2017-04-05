aaa84cc969c32dea079a7ec3d62da06cf5414347
Geison Camara <geison.camara@digitaldesk.com.br>
2017-04-05 09:35:18 -0300

    Ajustes link

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,5 +1,5 @@
-var url = 'http://localhost:5000/';
-//var url = 'http://192.168.1.156:3000/';
+//var url = 'http://localhost:5000/';
+var url = 'http://192.168.1.156:3000/';
 
 function setRouter(elem, action){
 	$.get(url+action, function(){
